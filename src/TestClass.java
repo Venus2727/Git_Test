35750a3f7ab9ce923bc9be861a2f0a0a6ea9fd45
Venus2727 <69390510+Venus2727@users.noreply.github.com>
2020-08-08 11:35:05 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Github dan merhaba!");
+        System.out.println("Git den merhaba!");
         
     }
 }
